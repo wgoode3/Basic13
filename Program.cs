001dd2d355dbb15cc613ecfbc79cb2e8f4dc8b03
William Goode <wgoode3@gmail.com>
2019-10-28 13:55:21 -0400

    initial

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Basic13
+{
+    class Program
+    {
+
+        static void Print1to255()
+        {
+            Console.WriteLine("I am coming from a method!");
+        }
+
+        static void PrintOdds1to255()
+        {
+
+        }
+
+        static void PrintIntsAndSums1to255()
+        {
+
+        }
+
+        static void PrintArray(List<int> arr)
+        {
+
+        }
+
+        static void PrintMax(List<int> arr) 
+        {
+
+        }
+
+        static void PrintAverage(List<int> arr)
+        {
+
+        }
+
+        static List<int> OddArray() {
+            List<int> oddArray = new List<int>();
+            // add all odd values from 1 to 255 to oddArray
+            return oddArray;
+        }
+
+        public void greaterThanY(List<int> arr, int y)
+        {
+
+        } 
+
+        public void squareArrayValues(List<int> arr)
+        {
+
+        }
+
+        public void setNegativesToZero(List<int> arr)
+        {
+
+        }
+
+        public void ShiftValuesToRight(List<int> arr) 
+        {
+            // Given an integer array, say [1, 5, 10, 7, -2], 
+            // Write a function that shifts each number by one to the front and adds '0' to the end. 
+            // For example, when the program is done, if the array [1, 5, 10, 7, -2] is passed to the function, 
+            // it should become [5, 10, 7, -2, 0].
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> exampleArr = new List<int>() {
+                3,
+                -4,
+                7,
+                12,
+                -11,
+                0,
+                -2
+            };
+            Print1to255();
+        }
+    }
+}
